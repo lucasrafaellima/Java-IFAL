db365ad2f6610441d5864dbe41dfd15a6f189b7d
lucasrafaellima <lucasrafaellima03@gmail.com>
2026-08-03 09:47:11 -0300

    Add implementation for atividade4 class

---
+++ lista2/atividade4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class atividade4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        int num = 0;
+
+        for (int count =0; count < 10; count++) {
+            System.out.print("Digite o numero " + count + ": ");
+            int num2 = sc.nextInt();
+            if (num2 <= num) {
+                num = num;
+            } else {
+                if (num2 > num) {
+                    num = num2;     
+                }
+            }
+        }
+        System.out.println("Maior numero digitado: " + num);
+        sc.close();
+    }
+}
