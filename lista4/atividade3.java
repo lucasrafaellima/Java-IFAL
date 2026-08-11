f72853a7ad1aa1f15a990d617726d368f86e7ad2
lucasrafaellima <87723096+lucasrafaellima@users.noreply.github.com>
2026-08-11 16:45:17 -0300

    Implement imposto calculation and user interaction in atividade3; add main method to atividade4 for area and perimeter calculations; create empty classes for atividade5, atividade6, and atividade7.

--- lista4/atividade3.java
+++ lista4/atividade3.java
@@ -1,5 +1,43 @@
 package lista4;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class atividade3 {
-    
+
+    public double imposto(double v) {
+        if (v < 2000) {
+            return 0;
+        } else if (v < 3500) {
+            return (v * 15) / 100;
+        } else if (v < 5000) {
+            return (v * 22) / 100; 
+        } else {
+            return (v * 30) / 100;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        atividade3 calc = new atividade3();
+
+        System.out.print("Digite o valor que deseja calcular o imposto: ");
+        double valor = sc.nextDouble();
+
+        if (valor < 2000) {
+            System.out.println("Valor isento! " + calc.imposto(valor));
+            calc.imposto(valor);
+        } else if (valor < 3500) {
+            System.out.println("Imposto de 15%! " + calc.imposto(valor));
+            calc.imposto(valor);
+        } else if (valor < 5000) {
+            System.out.println("Imposto de 22%! " + calc.imposto(valor));
+            calc.imposto(valor);
+        } else {
+            System.out.println("Imposto de 30%! " + calc.imposto(valor));
+        }
+
+        sc.close();
+
+    }    
 }
--- lista4/atividade4.java
+++ lista4/atividade4.java
@@ -1,5 +1,45 @@
 package lista4;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class atividade4 {
-    
+
+    public int area(int b, int h) {
+        return b * h;
+    }
+
+    public int perimetro(int b, int h) {
+        return (2*b) + (2*h);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        atividade4 calc = new atividade4();
+
+        System.out.print("Digite o valor da base: ");
+        int base = sc.nextInt();
+        System.out.print("Digite o valor da altura: ");
+        int altura = sc.nextInt();
+
+        int escolha = -1;
+
+        while (escolha != 0) {
+            System.out.print("Digite uma das opções: \n 1 - calcular area \n 2- calcular perimetro \n 0- sair \n resposta: ");
+            escolha = sc.nextInt();
+
+            if (escolha == 1) {
+                System.out.println("O valor da área é: " + calc.area(base, altura));
+            } else if (escolha == 2) {
+                System.out.println("O valor do perimetro é: " + calc.perimetro(base, altura));
+            } else {
+                System.out.println("Saindo do sistema!");
+            }
+
+        }
+
+        sc.close();
+
+    }    
 }
