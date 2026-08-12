ebb950c24e3660d0da4db6ad88aee77124d82b1f
lucasrafaellima <87723096+lucasrafaellima@users.noreply.github.com>
2026-08-12 15:33:22 -0300

    Implement main method and user input for notas in atividade6

--- lista4/atividade6.java
+++ lista4/atividade6.java
@@ -1,5 +1,23 @@
 package lista4;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class atividade6 {
-    
+
+    public double mostrar() {
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double[] notas = new double[4];
+
+        for (int i =0; i < notas.length; i++) {
+            System.out.print("Digite a nota de ordem " + i + ": ");
+            notas[i] = sc.nextDouble();
+        }
+
+    }    
 }
