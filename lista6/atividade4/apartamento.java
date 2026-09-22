1077b66a7b2e65bc32003e43c55a534a2f2c0a59
lucasrafaellima <87723096+lucasrafaellima@users.noreply.github.com>
2026-09-22 16:51:59 -0300

    Update apartamento and casa classes: modify constructors to include apartamento parameter for improved property management functionality.

--- lista6/atividade4/apartamento.java
+++ lista6/atividade4/apartamento.java
@@ -3,8 +3,8 @@ package lista6.atividade4;
 public class apartamento extends imovel {
     private String nomePredio;
 
-    public apartamento(String rua, String bairro, String cidade, String estado, String cep, double preco, boolean mobiliado, boolean alugado, String nomePredio) {
-        super(rua, bairro, cidade, estado, cep, preco, mobiliado, alugado);
+    public apartamento(String rua, String bairro, String cidade, String estado, String cep, double preco, boolean mobiliado, boolean alugado, String nomePredio, apartamento apartamento) {
+        super(rua, bairro, cidade, estado, cep, preco, mobiliado, alugado, apartamento);
         this.nomePredio = nomePredio;
     }
 
--- lista6/atividade4/casa.java
+++ lista6/atividade4/casa.java
@@ -2,8 +2,8 @@ package lista6.atividade4;
 
 public class casa extends imovel {
 
-    public casa(String rua, String bairro, String cidade, String estado, String cep, double preco, boolean mobiliado, boolean alugado) {
-        super(rua, bairro, cidade, estado, cep, preco, mobiliado, alugado);
+    public casa(String rua, String bairro, String cidade, String estado, String cep, double preco, boolean mobiliado, boolean alugado, apartamento apartamento) {
+        super(rua, bairro, cidade, estado, cep, preco, mobiliado, alugado, apartamento);
     }
     
 }
